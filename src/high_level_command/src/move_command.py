c2f566b15272f3f3d2f3e19105f6019bd513f6a7
Your Name <test@gmail.com>
2022-03-12 14:00:55 +0000

    can move in a square

--- src/high_level_command/src/move_command.py
+++ src/high_level_command/src/move_command.py
@@ -51,6 +51,25 @@ if __name__ == '__main__':
    rospy.init_node('map_navigation', anonymous=False)
    x_goal = 0.3
    y_goal = 1.5
+   x1 = 1
+   y1 = 1
+   x2 = 1
+   y2 = 0
+   x3 = 0
+   y3 = 0
+   x4 = 0
+   y4 = 1
    print("start go to goal")
    move_to_goal(x_goal,y_goal)
+   rospy.sleep(3) # Sleeps for 3 sec
+
+   move_to_goal(x1,y1)
+   rospy.sleep(3) # Sleeps for 3 sec
+   move_to_goal(x2,y2)
+   rospy.sleep(3) # Sleeps for 3 sec
+   move_to_goal(x3,y3)
+   rospy.sleep(3) # Sleeps for 3 sec
+   move_to_goal(x4,y4)
+   rospy.sleep(3) # Sleeps for 3 sec
+
    rospy.spin()
